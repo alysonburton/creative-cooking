697045e3236d1394ea8df62dc92b7e2c54bf529f
veg4lyfe <burton.alyson9@gmail.com>
2016-03-16 17:47:50 -0700

    fixed error

--- public/index.php
+++ public/index.php
@@ -14,4 +14,4 @@
 define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
-echo "echo eecho"; ?>
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
--- public/wp-content/themes/creativecooking/sidebar-cats.php
+++
@@ -1,3 +0,0 @@
-cats cats cats cats cats catscats catscats catscats catscats catscats catscats cats
-cats cats
-cats catscats cats cats catscats catsvcats cats
